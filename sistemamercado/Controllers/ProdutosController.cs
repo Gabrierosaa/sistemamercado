8e9f344f0d2895fcbb253c0fe3aa09319e9c5e88
Gabriel <gabrielflororosa2007@gmail.com>
2026-01-03 15:30:14 -0300

    Adicionar arquivos de projeto.

---
+++ sistemamercado/Controllers/ProdutosController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace sistemamercado.Controllers
+{
+
+    [Route("produtos")]
+    public class ProdutosController : Controller
+    {
+        string nome = "Rota de Produtos funcionando";
+        public IActionResult Index()
+        {
+            return Content(nome);
+        }
+    }
+}
